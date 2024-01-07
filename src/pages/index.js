84f83cf5d7e00b8eb8d83594b3560d54acbdcc0b
drPKwiecien <dr.pkwiecien@gmail.com>
2024-01-07 09:39:45 +0100

    dodanie textContent.js i import tekstów z tego pliku do index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import kartingText from '../textContent';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,7 +10,7 @@ export default function Home() {
     <main className={`flex min-h-screen items-center justify-center bg-white mt-12 mb-12 ml-36 mr-36 ${inter.className}`}>
       <div className="flex items-center justify-center w-full">
         <div className="text-left p-8 flex-grow flex-basis-2/3">
-          <p className="text-black">Karting to wspaniały sport, dający możliwość posmakowania motorsportowych emocji każdej osobie. Jednak doświadczenia mogą być bardzo różne. Wpływ ma na to wiele czynników takich jak układ i długość trasy, możliwości i stan gokartów, udogodnienia w obiekcie, a także oczywiście… cena! Chcesz dowiedzieć się, które tory warto odwiedzić, a których lepiej unikać? Zapraszam do jedynej w Polsce porównywarki torów gokartowych!</p>
+          <p className="text-black">{kartingText}</p>
         </div>
         <div className="p-4 flex-grow flex-basis-1/3">
           <Image src="/kartmainpic.png" alt="Main Karting Image" width={1200} height={600} />
---
+++ src/textContent.js
@@ -0,0 +1,4 @@
+const kartingText = `Karting to wspaniały sport, dający możliwość posmakowania motorsportowych emocji każdej osobie w przystępny sposób. 
+Jednak doświadczenia mogą być bardzo różne. Wpływ ma na to wiele czynników takich jak układ i długość trasy, możliwości i stan gokartów, udogodnienia w obiekcie, 
+a także oczywiście… cena! Chcesz dowiedzieć się, które tory warto odwiedzić, a których lepiej unikać? Zapraszam do jedynej w Polsce porównywarki torów gokartowych!`;
+export default kartingText;
