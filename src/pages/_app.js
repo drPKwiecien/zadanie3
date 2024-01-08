cce092bf39da79b1761e081ac4ca73e1f7fa0852
drPKwiecien <dr.pkwiecien@gmail.com>
2024-01-08 20:29:26 +0100

    dodanie navbarbottom do app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import Navbarbottom from '../components/Navbarbottom'
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Navbarbottom />
+      <Component {...pageProps} />
+    </>
+  ) 
 }
