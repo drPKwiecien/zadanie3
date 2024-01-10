59e5f0731f462b099822e32b89541c56d9cc0e73
drPKwiecien <dr.pkwiecien@gmail.com>
2024-01-10 19:16:36 +0100

    dodanie komponentu Image do karuzeli

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carousel CSS
+import Image from 'next/image'
 
 export default function ImageCarousel({ images }) {
     return (
       <Carousel>
         {images.map((image, index) => (
-          <div key={index} style={{ padding: '5px' }}> {/* Add padding here */}
-            <img src={image.src} alt={image.alt} style={{ width: 'auto', height: 'auto' }} />
+          <div key={index} style={{ padding: '8px' }}> {/* Add padding here */}
+            <Image 
+              src={image.src} 
+              alt={image.alt} 
+              width={500} 
+              height={500} 
+              layout="responsive" 
+            />
           </div>
         ))}
       </Carousel>
     );
-  }
-  
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,11 +6,12 @@ import Navbarbottom from '../components/Navbarbottom'
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Navbarbottom />
+      
       <div className="body-pattern">
         <div className="central-container flex flex-1 items-center justify-center bg-white  min-h-screen">
           <Component {...pageProps} />
         </div>
+        <Navbarbottom />
       </div>
     </>
   );
