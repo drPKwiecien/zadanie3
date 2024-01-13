50cdb06d1b8d95c45783992716fbe628417c1b8f
drPKwiecien <dr.pkwiecien@gmail.com>
2024-01-13 09:54:21 +0100

    Modyfikacja kodu regulującego kontener główny
    Ustawiono sreen xl, usunięcie niepotrzebnych elementów z globals.css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,13 +6,12 @@ import Navbarbottom from '../components/Navbarbottom'
 export default function App({ Component, pageProps }) {
   return (
     <>
-      
-      <div className="body-pattern">
-        <div className="central-container flex flex-1 items-center justify-center bg-white  min-h-screen">
-          <Component {...pageProps} />
-        </div>
-        <div className='navbar-bottom'>
-          <Navbarbottom />
+      <div className='body-pattern'>
+        <div className="max-w-screen-xl mx-auto">
+          <div className="flex flex-1 items-center justify-center bg-white min-h-screen">
+            <Component {...pageProps} />
+          </div>
+            <Navbarbottom />
         </div>
       </div>
     </>
