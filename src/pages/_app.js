da392f1842e7f45f6067239732fffa9b0e01b847
drPKwiecien <dr.pkwiecien@gmail.com>
2024-01-13 18:25:16 +0100

    Ustawienie Navbarbottom sztywno na dole
    po co: szczególnie na mobile dotarcie do komponentu wymaga sporo scrollowania, utrudniając nawigację użytkownikowi

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,23 @@
 import '@/styles/globals.css'
 import Navbarbottom from '../components/Navbarbottom'
 
-
-
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <div className='body-pattern'>
-        <div className="max-w-screen-xl mx-auto">
-          <div className="flex flex-1 items-center justify-center bg-white min-h-screen">
-            <Component {...pageProps} />
+    <div className="flex flex-col h-screen">
+      <div className="flex-grow">
+        <div className='body-pattern'>
+          <div className="max-w-screen-xl mx-auto">
+            <div className="flex flex-1 items-center justify-center bg-white min-h-screen">
+              <Component {...pageProps} />
+            </div>
           </div>
+        </div>
+      </div>
+      <div className="fixed bottom-0 left-0 right-0">
+        <div className="max-w-screen-xl mx-auto w-full">
             <Navbarbottom />
         </div>
       </div>
-    </>
+    </div>
   );
 }
