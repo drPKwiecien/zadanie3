c744d1349267847b7b75b609196bf782c952d80c
drPKwiecien <dr.pkwiecien@gmail.com>
2024-01-13 10:14:23 +0100

    Usunięcie niepotrzebnych komponentów i przeniesienie pliku testowego do głównego rankingu
    Karuzela została zastąpiona wersją '2', zaś TrackTable wykorzystujący TrackButton został zastąpiony Tracklist

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 Obecne problemy
 
-1. Tabela w ranking nie skaluje się dobrze mobilnie - próbowałem komponentu DaisyUI ale też się nie skalował, a był gorszy (bez map dla indeksów)
-2. karuzela skaluje się vertykalnie do najwyższego obrazka, przez co jeśli przeskoczymy na mniejszy to jest ogromna biała przestrzeń - nie wiem jak sobie z tym poradzić, dobrze to widać na podstronie /Glass74
-3. Obrazki karuzeli przykrywają mi dolny navbar
-4. dolny tekst chowa się trochę pod navbarem
+
 
 Wyzwania:
 
-1. inny storage do contentu (obrazki, video, text)
-2. obecnie każda podstrona to nowy plik .js - jeśli w tabeli będzie 200 torów to drzewo bardzo niefajnie się rozbuduje - czy jest na to lepszy sposób?
+1. zastąpić docx plikiem tekstowym z korektą w VSC
 
--- src/pages/ranking.js
+++ src/pages/ranking.js
@@ -1,14 +1,16 @@
-import TrackTable from '../components/TrackTable';
+import Tracklist from '../components/Tracklist';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 
-export default function Ranking() {
-    return <main className={`flex-col flex-1 items-center ml-12 mr-12 ${inter.className}`}>
-      <header className="text-center mb-4">
-          <h1>Ranking Torów Kartingowych</h1>
-      </header>
-      <TrackTable />  
-    </main>
+export default function Test() {
+    return (
+      <main className={`flex-col items-center ml-8 mr-8 ${inter.className}`}>
+        <header className="text-center mb-4">
+            <h1>Ranking Torów Kartingowych</h1>
+        </header>
+          <Tracklist />
+      </main>
+    )
   }
