d2830285d5062e27e2d33923f2ec22b6c5a22fcb
drPKwiecien <dr.pkwiecien@gmail.com>
2024-01-13 18:23:57 +0100

    Zmiana wyglądu strony głównej
    1. poprawienie guzika, aby przechowywał placeholder
    2. przesuwanie obrazka na dół na mobile w celu lepszej widoczności

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,10 @@
 import Link from 'next/link';
 
 
-export default function Button({href}) {
+export default function Button({href,copy}) {
     return (
         <Link href={href} passHref>
-            <button className="btn bg-oceanBlue text-white">Znajdź najlepszy tor!</button>
+            <button className="btn bg-oceanBlue text-white">{copy}</button>
         </Link>    
     );
 }
