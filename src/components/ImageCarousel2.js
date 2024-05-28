06af6ef51da4464e34a0f294c2ac65a298d1c795
drPKwiecien <dr.pkwiecien@gmail.com>
2024-05-28 19:32:08 +0200

    dodanie updatu indeksu karuzeli przy swipe - korekta3

--- src/components/ImageCarousel2.js
+++ src/components/ImageCarousel2.js
@@ -1,60 +1,56 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import Image from 'next/image';
 import ReactPlayer from 'react-player/lazy'; // Lazy load the video player
 
 export default function ImageCarousel2({ images }) {
-
   const [activeIndex, setActiveIndex] = useState(0); // State to track active image
   const [isMuted, setIsMuted] = useState(true);
+  const carouselRef = useRef(null);
 
   // Helper function to determine if the URL is a video based on the extension
-  const isVideo = (url) => {
-    return /\.(mp4|webm|ogg)$/i.test(url);
-  };
+  const isVideo = (url) => /\.(mp4|webm|ogg)$/i.test(url);
 
-  const isLap = (url) => {
-    return /LAP.*\.mp4$/i.test(url);
-  };
+  const isLap = (url) => /LAP.*\.mp4$/i.test(url);
 
-  const shouldDisplayItem = (url) => {
-    return !isLap(url);
-  };
+  const shouldDisplayItem = (url) => !isLap(url);
 
   const handleVideoError = (e) => {
     console.error('Error playing video:', e);
   };
 
-  const handleTouchEnd = useCallback((event) => {
-    const carousel = event.currentTarget;
-    const items = Array.from(carousel.children);
-    const currentItemIndex = items.findIndex(
-      (item) => item.classList.contains('carousel-item-active')
-    );
-
-    if (currentItemIndex !== -1) {
-      setActiveIndex(currentItemIndex);
-    }
+  const updateActiveIndex = useCallback((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const currentIndex = Number(entry.target.dataset.index);
+        setActiveIndex(currentIndex);
+      }
+    });
   }, []);
 
   useEffect(() => {
-    const carouselItems = document.querySelectorAll('.carousel-item');
-    carouselItems.forEach((item) => {
-      item.addEventListener('touchend', handleTouchEnd);
+    const observer = new IntersectionObserver(updateActiveIndex, {
+      root: carouselRef.current,
+      threshold: 0.5,
     });
 
-    return () => {
-      carouselItems.forEach((item) => {
-        item.removeEventListener('touchend', handleTouchEnd);
-      });
-    };
-  }, [handleTouchEnd]);
+    const items = carouselRef.current.querySelectorAll('.carousel-item');
+    items.forEach((item) => observer.observe(item));
 
+    return () => {
+      items.forEach((item) => observer.unobserve(item));
+    };
+  }, [updateActiveIndex]);
 
   return (
     <div>
-      <div className="w-96 carousel rounded-box">
-      {images.filter(({ src }) => shouldDisplayItem(src)).map((media, index) => (
-          <div key={index} id={`item${index + 1}`} className="carousel-item w-full">
+      <div ref={carouselRef} className="w-96 carousel rounded-box">
+        {images.filter(({ src }) => shouldDisplayItem(src)).map((media, index) => (
+          <div
+            key={index}
+            id={`item${index + 1}`}
+            className="carousel-item w-full"
+            data-index={index}
+          >
             {!isVideo(media.src) ? (
               // Render an image if the media is not a video
               <Image
@@ -66,13 +62,13 @@ export default function ImageCarousel2({ images }) {
               />
             ) : (
               // Render a video if the media is a video but does not contain 'LAP' in name
-              <div className='player-wrapper'>
+              <div className="player-wrapper">
                 <ReactPlayer
                   url={media.src}
-                  className='react-player'
+                  className="react-player"
                   playing={true}
-                  width='100%'
-                  height='100%'
+                  width="100%"
+                  height="100%"
                   controls={true}
                   playsInline={true} // Correct attribute for inline playback in JSX
                   muted={isMuted}
@@ -85,15 +81,18 @@ export default function ImageCarousel2({ images }) {
       </div>
 
       <div className="flex justify-center w-full py-2 gap-2">
-      {images.filter(({ src }) => shouldDisplayItem(src)).map((_, index) => (
-          <a 
-          key={index} 
-          href={`#item${index + 1}`} 
-          className={`btn btn-xs ${index === activeIndex ? 'bg-oceanBlue' : ''}`}
-          onClick={() => setActive(index)} // Set active index on click
-          >{index + 1}</a>
+        {images.filter(({ src }) => shouldDisplayItem(src)).map((_, index) => (
+          <a
+            key={index}
+            href={`#item${index + 1}`}
+            className={`btn btn-xs ${index === activeIndex ? 'bg-oceanBlue' : ''}`}
+            onClick={() => setActiveIndex(index)} // Set active index on click
+          >
+            {index + 1}
+          </a>
         ))}
       </div>
     </div>
   );
 }
+
