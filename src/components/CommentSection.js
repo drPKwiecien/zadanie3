efe3ad2f3a13d8940f6770d085ffd8cd098995a2
ranz7 <rdslw@student.agh.edu.pl>
2024-09-01 17:42:51 +0200

    Dodano odstępy pomiędzy komentarzami

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -2,7 +2,7 @@ import { Comment } from "./Comment"
 
 export function CommentSection({ comments, parent }) {
     return comments[parent].map(comment => (
-        <div key={comment.id} >
+        <div key={comment.id} className="pt-2">
             <Comment {...comment} comments={comments}/>
         </div>
     ))
